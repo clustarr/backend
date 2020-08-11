eb127824db0dc16572a8e2bbc5ff46523b9d00c3
lucasheld <lucasheld@hotmail.de>
2020-08-11 18:47:30 +0200

    create docker-compose config to run redis, celery and flask

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
-ANSIBLE_PROJECT_PATH = "/home/lucas/Projects/clustarr/ansible"
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+import os
+
+ANSIBLE_PROJECT_PATH = os.environ.get("ANSIBLE_PROJECT_PATH", "/home/lucas/Projects/clustarr/ansible")
+CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379/0")
+CELERY_RESULT_BACKEND = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379/0")
--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,5 @@
 from main import app
 
 
-# command to run app in production:
-# gunicorn --bind 0.0.0.0:5000 wsgi:app
-
 if __name__ == "__main__":
     app.run()
